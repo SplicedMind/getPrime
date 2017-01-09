38f234c55103efb73eaa593b6395b595c933b2f1
Akinbode Femi <femi661@gmail.com>
2017-01-09 07:41:43 +0100

    Committing new files

---
+++ app/getPrime.js
@@ -0,0 +1,45 @@
+module.exports = {
+ 
+   getPrime: function(num) {
+      var prime = [];
+      var iterate = 1;
+      var j;      
+
+      if (num >= 2)
+      {
+         if(num == 2)
+         {
+            prime.push(2);
+         }
+         else if(num >= 3)
+         {
+            prime.push(2);            
+            for(var i = 0; i < num -2; i += 2)
+            {
+               prime.push(3 + i);
+            }
+         }
+         var limit = Math.sqrt(num);
+         var primelength = prime.length;
+         while(iterate < primelength - 1)
+         {
+            if(prime[iterate] <= limit)
+            {
+               j = 1 + iterate;
+               while(j < primelength)
+               {
+                  if(prime[j] % prime[iterate] == 0)
+                  {
+                    prime.splice(j,1);
+                  }   
+                      j += 1;
+               }
+            }
+            primelength = prime.length;
+            iterate += 1;
+         }
+      }
+      return prime;
+   }
+
+}
